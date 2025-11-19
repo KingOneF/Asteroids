31c99921955989905e92aa55db9ba320075fc648
KingOneF <jefwilkins@gmail.com>
2025-11-19 13:01:04 -0600

    asteroids prep

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pygame
 
-pygame.init()
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
-# Create a window
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Asteroids")
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
-# Simple game loop
-running = True
-clock = pygame.time.Clock()
+if __name__ == "__main__":
+    main()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-    clock.tick(60)
-
-pygame.quit()
+#
+# pygame.init()
+#
+# # Create a window
+# screen = pygame.display.set_mode((800, 600))
+# pygame.display.set_caption("Asteroids")
+#
+# # Simple game loop
+# running = True
+# clock = pygame.time.Clock()
+#
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#
+#     screen.fill((0, 0, 0))
+#     pygame.display.flip()
+#     clock.tick(60)
+#
+# pygame.quit()
